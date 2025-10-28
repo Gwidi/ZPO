63563d8d443a780954fe0d75a417e7ed25c573a9
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-10-28 16:31:47 +0100

    Add augmentation to training dataset

--- lab02.py
+++ lab02.py
@@ -18,6 +18,18 @@ def main():
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
+    # Define augmentations for training data (5 different) - task 5 
+    augmentations = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+        transforms.RandomVerticalFlip(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
     # Load the OxfordIIITPet dataset
     train_dataset = OxfordIIITPet(root='./data', split='trainval', download=True, transform=transform)
     test_dataset = OxfordIIITPet(root='./data', split='test', download=True, transform=transform)
@@ -26,6 +38,8 @@ def main():
     print(f"Number of test samples: {len(test_dataset)}")
 
     train_OxfordIIITPet, val_OxfordIIITPet = torch.utils.data.random_split(train_dataset, [0.75, 0.25])
+    # Apply augmentations only to training dataset
+    train_OxfordIIITPet.dataset.transform = augmentations
 
     train_loader = torch.utils.data.DataLoader(train_OxfordIIITPet, batch_size=64, num_workers=2)
     val_loader = torch.utils.data.DataLoader(val_OxfordIIITPet, batch_size=64, num_workers=2)
@@ -42,7 +56,7 @@ def main():
     # Exercise 2 Train the model and verify its performance on the test set.
     trainer = L.Trainer(max_epochs=10, accelerator='gpu')
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
-    trainer.test(model, test_dataloaders=test_loader)
+    trainer.test(model, test_loader)
 
 
 if __name__ == "__main__":
